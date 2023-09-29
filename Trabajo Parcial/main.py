2f63a7e2e705743dc228c65f196008c49ae14a91
Daniel Barrionuevo <116469158+danieleub02@users.noreply.github.com>
2023-09-28 23:27:29 -0500

    contador

--- Trabajo Parcial/main.py
+++ Trabajo Parcial/main.py
@@ -8,9 +8,12 @@ with open('espacios.pkl', 'rb') as file:
 
 video = cv2.VideoCapture('video2.mp4')
 
-contador_recuadros_ocupado = 0
+b = []
+cc = 0
+veces = 0
 
 while True:
+    cc += 1
     check, img = video.read()
     imgBN = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgTH = cv2.adaptiveThreshold(imgBN, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
@@ -24,12 +27,15 @@ while True:
         cv2.putText(img, str(count), (x,y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1)
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
         if count < 1800:
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)        
         else:
-            contador_recuadros_ocupado += 1
+            b.append(cc)
+            if len(b)>1:
+                if b[-1] != (b[-2]+1):
+                    veces += 1
 
 
-    texto = f'Recuadro Ocupado: {contador_recuadros_ocupado}'
+    texto = f'Recuadro Ocupado: {veces}'
     cv2.putText(img, texto, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
     cv2.imshow('video2', img)
